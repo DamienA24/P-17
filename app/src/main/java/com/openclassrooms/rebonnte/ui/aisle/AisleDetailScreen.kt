95c999cc47ac4e15e2be87cb19c3b5d1f4f5609c
DamienA <alves.d24@laposte.net>
2026-03-28 10:13:32 +0100

    refactor: extract AisleDetailViewModel to its own file

--- app/src/main/java/com/openclassrooms/rebonnte/ui/aisle/AisleDetailScreen.kt
+++ app/src/main/java/com/openclassrooms/rebonnte/ui/aisle/AisleDetailScreen.kt
@@ -16,36 +16,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.openclassrooms.rebonnte.data.repository.MedicineRepository
-import com.openclassrooms.rebonnte.model.Medicine
 import com.openclassrooms.rebonnte.ui.medicine.MedicineItem
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import javax.inject.Inject
-
-@HiltViewModel
-class AisleDetailViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-    private val repo: MedicineRepository
-) : ViewModel() {
-
-    private val aisleId: String = checkNotNull(savedStateHandle["aisleId"])
-
-    private val _medicines = MutableStateFlow<List<Medicine>>(emptyList())
-    val medicines: StateFlow<List<Medicine>> = _medicines.asStateFlow()
-
-    init {
-        viewModelScope.launch {
-            repo.getMedicinesByAisle(aisleId).collect { _medicines.value = it }
-        }
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
