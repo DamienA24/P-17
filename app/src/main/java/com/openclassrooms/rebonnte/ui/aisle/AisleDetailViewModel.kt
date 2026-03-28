474c316754e403152bbf56cf9c44acbef53d8087
DamienA <alves.d24@laposte.net>
2026-03-28 10:13:32 +0100

    refactor: extract AisleDetailViewModel to its own file

---
+++ app/src/main/java/com/openclassrooms/rebonnte/ui/aisle/AisleDetailViewModel.kt
@@ -0,0 +1,31 @@
+package com.openclassrooms.rebonnte.ui.aisle
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.openclassrooms.rebonnte.data.repository.MedicineRepository
+import com.openclassrooms.rebonnte.model.Medicine
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AisleDetailViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val repo: MedicineRepository
+) : ViewModel() {
+
+    private val aisleId: String = checkNotNull(savedStateHandle["aisleId"])
+
+    private val _medicines = MutableStateFlow<List<Medicine>>(emptyList())
+    val medicines: StateFlow<List<Medicine>> = _medicines.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            repo.getMedicinesByAisle(aisleId).collect { _medicines.value = it }
+        }
+    }
+}
