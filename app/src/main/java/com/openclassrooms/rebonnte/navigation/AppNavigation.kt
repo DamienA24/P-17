ca1292fc20007e1830a4573d74ac8e332a63c85c
DamienA <alves.d24@laposte.net>
2026-03-28 10:57:19 +0100

    refactor: extract AppNavigation to navigation package, slim down MainActivity

---
+++ app/src/main/java/com/openclassrooms/rebonnte/navigation/AppNavigation.kt
@@ -0,0 +1,21 @@
+package com.openclassrooms.rebonnte.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.openclassrooms.rebonnte.ui.auth.LoginScreen
+import com.openclassrooms.rebonnte.ui.theme.RebonnteTheme
+
+@Composable
+fun AppNavigation(isLoggedIn: Boolean) {
+    RebonnteTheme {
+        var loggedIn by remember { mutableStateOf(isLoggedIn) }
+        if (loggedIn) {
+            AuthenticatedShell()
+        } else {
+            LoginScreen(onLoginSuccess = { loggedIn = true })
+        }
+    }
+}
