0bf7b8314594ddd7f60f2a61a1d60c33c110d9c2
DamienA <alves.d24@laposte.net>
2026-04-09 21:58:37 +0200

    fix(auth): drive app navigation reactively from authStateFlow via AppViewModel

--- app/src/main/java/com/openclassrooms/rebonnte/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/rebonnte/MainActivity.kt
@@ -3,20 +3,15 @@ package com.openclassrooms.rebonnte
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.google.firebase.auth.FirebaseAuth
 import com.openclassrooms.rebonnte.navigation.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppNavigation(isLoggedIn = auth.currentUser != null)
+            AppNavigation()
         }
     }
 }
